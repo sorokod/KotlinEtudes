5a057a6cab6540b4edaecd900db9bf9c58837790
David Soroko <david@picamail.eu>
2021-05-09 16:03:27 +0200

    e NQueens

--- src/main/nqueens/NQueens.kt
+++ src/main/nqueens/NQueens.kt
@@ -3,36 +3,53 @@ package nqueens
 import kotlin.math.abs
 
 
-const val N: Int = 4
+const val SIZE: Int = 4
 
-data class Queen(val col: Int, val row: Int = 0)
+data class Queen(val col: Int, val row: Int)
 
-val queens = mutableListOf<Queen>()
+/**
+ * A convenience object for keeping track, adding and removing queens
+ */
+object Queens {
+    private val queens = mutableListOf<Queen>()
 
-fun valid(q: Queen, queens: List<Queen>) = queens.all { valid(it, q) }
-fun valid(q1: Queen, q2: Queen): Boolean =
+    fun add(newQ: Queen): Boolean = valid(newQ) && queens.add(newQ)
+
+    fun removeLast() = queens.removeLast()
+
+    fun isSolved() = queens.size == SIZE
+
+    fun print() {
+        val board = IntArray(SIZE * SIZE)
+        queens.forEach { board[it.row * SIZE + it.col] = 1 }
+
+        board.forEachIndexed { i, k ->
+            if (i.rem(SIZE) == 0) println()
+            print("$k ")
+        }
+    }
+
+    private fun valid(newQ: Queen) = queens.all { q -> valid(q, newQ) }
+    private fun valid(q1: Queen, q2: Queen): Boolean =
         when {
-            q1.row == q2.row ||
-            q1.col == q2.col ||
-            abs(q1.row - q2.row) == abs(q1.col - q2.col) -> false
+            q1.row == q2.row || q1.col == q2.col || abs(q1.row - q2.row) == abs(q1.col - q2.col) -> false
             else -> true
         }
+}
 
-fun solve(col: Int): Boolean {
-    if (queens.size == N) {
+val queens = Queens
+
+fun solveColumnsStartingFrom(col: Int): Boolean {
+    if (queens.isSolved()) {
         return true
     }
 
-    (0 until N).forEach {
-        val queen = Queen(col, it)
-
-        if (valid(queen, queens)) {
-            queens.add(queen)
-
-            if (solve(col + 1)) {
+    (0 until SIZE).forEach { row ->
+        if (queens.add(Queen(col, row))) {
+            if (solveColumnsStartingFrom(col + 1)) {
                 return true
             } else {
-                queens.removeAt(queens.lastIndex)
+                queens.removeLast()
             }
         }
     }
@@ -40,16 +57,10 @@ fun solve(col: Int): Boolean {
 }
 
 fun printSolution() {
-    val board = IntArray(N * N)
-    queens.forEach { board[it.row * N + it.col] = 1 }
-
-    board.forEachIndexed { i, k ->
-        if (i.rem(N) == 0) println()
-        print("$k ")
-    }
+    Queens.print()
 }
 
 fun main() {
-    solve(0)
+    solveColumnsStartingFrom(0)
     printSolution()
 }
