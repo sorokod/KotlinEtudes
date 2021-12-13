447456b0a4809a916645b0c62d119be052d01986
David Soroko <david@picamail.eu>
2021-12-13 21:48:19 +0100

    Added mockServer.verify(...)

--- retrofittestdrive/src/test/kotlin/retrofittestdrive/RetrofitTest.kt
+++ retrofittestdrive/src/test/kotlin/retrofittestdrive/RetrofitTest.kt
@@ -8,8 +8,8 @@ import org.mockserver.matchers.TimeToLive
 import org.mockserver.matchers.Times
 import org.mockserver.model.HttpRequest.request
 import org.mockserver.model.HttpResponse.response
+import org.mockserver.verify.VerificationTimes.*
 import retrofit2.Call
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.SECONDS
 
 
@@ -25,43 +25,6 @@ internal class RetrofitTest {
             mockServer = startClientAndServer(mockServerPort)
             println("XXX MockServer is up: ${mockServer.isRunning} on port: ${mockServer.port}")
 
-            mockServer.`when`(
-                request().withMethod("GET").withPath("/users/octocat/repos")
-            ).respond(
-                response()
-                    .withStatusCode(200)
-                    .withBody(""" [{ "id": 1296269, "name": "octocat", "owner": {} }] """.trimIndent())
-            )
-
-            // ######################################
-
-            mockServer.`when`(
-                request().withMethod("GET").withPath("/users/good"),
-            ).respond(
-                response()
-                    .withStatusCode(200)
-                    .withBody("""[{ "id": 1296269, "name": "good", "owner": {"login": "good"} }]""")
-            )
-
-            // quirky - use priority to simulate x2 500 errors followed by 200
-            mockServer.`when`(
-                request()
-                    .withMethod("GET").withPath("/users/quirky"),
-                Times.exactly(2), TimeToLive.exactly(SECONDS, 60L), 100
-            ).respond(
-                response()
-                    .withStatusCode(500)
-            )
-
-            mockServer.`when`(
-                request()
-                    .withMethod("GET").withPath("/users/quirky"),
-                Times.exactly(2), TimeToLive.exactly(SECONDS, 60L), 1
-            ).respond(
-                response()
-                    .withStatusCode(200)
-                    .withBody("""[{ "id": 1296269, "name": "quirky", "owner": {"login": "quirky"} }]""")
-            )
         }
 
         @AfterAll
@@ -72,20 +35,24 @@ internal class RetrofitTest {
     }
 
     private val baselineClient = buildBaselineClient(GitHubApi::class.java, "http://localhost:1080/")
-    private val resilientClientX = buildResilientClient(LocalTestApi::class.java, "http://localhost:1080/")
+    private val resilientClient = buildResilientClient(LocalTestApi::class.java, "http://localhost:1080/")
 
     @Test
     fun `baseline - matching requests = 200`() {
+        mockServer.scenario_Octocat_200()
+
         val call: Call<List<Repo>> = baselineClient.listRepos("octocat")
         val response = call.execute()
-        val result: List<Repo>? = response.body()
+        val result: List<Repo> = response.body()!!
 
         assertEquals(200, response.code())
-        assertEquals(1, result!!.size)
+        assertEquals(1, result.size)
     }
 
     @Test
     fun `baseline - non matching requests = 404`() {
+        mockServer.scenario_Octocat_200()
+
         baselineClient.listRepos("bad cat!").execute().also { response ->
             assertEquals(404, response.code())
         }
@@ -95,7 +62,9 @@ internal class RetrofitTest {
 
     @Test
     fun `resilient - 200`() {
-        resilientClientX.getLocal(id = "good").execute().also { response ->
+        mockServer.scenario_Good_200()
+
+        resilientClient.getLocal(id = "good").execute().also { response ->
             assertEquals(200, response.code())
             assertEquals("good", response.body()!![0].name)
         }
@@ -103,20 +72,67 @@ internal class RetrofitTest {
 
     @Test
     fun `resilient - 404`() {
-        resilientClientX.getLocal(id = "not good").execute().also { response ->
-            assertEquals(404, response.code())
-//            assertEquals("not good", mockServer.verify(request().withPath("/users/quirky"), exactly(1)))
+        mockServer.scenario_Good_200()
 
+        resilientClient.getLocal(id = "not_good").execute().also { response ->
+            assertEquals(404, response.code())
+            mockServer.verify(request().withPath("/users/not_good"), exactly(1))
         }
     }
 
     @Test
     fun `resilient - quirky`() {
-        resilientClientX.getLocal(id = "quirky").execute().also { response ->
+        mockServer.scenario_Quirky()
+
+        resilientClient.getLocal(id = "quirky").execute().also { response ->
             assertEquals(200, response.code())
             assertEquals("quirky", response.body()!![0].name)
+            mockServer.verify(request().withPath("/users/quirky"), exactly(3))
         }
     }
+}
 
+fun ClientAndServer.scenario_Octocat_200() {
+    this.reset()
+    this.`when`(
+        request().withMethod("GET").withPath("/users/octocat/repos")
+    ).respond(
+        response()
+            .withStatusCode(200)
+            .withBody(""" [{ "id": 1296269, "name": "octocat", "owner": {} }] """.trimIndent())
+    )
+}
 
+fun ClientAndServer.scenario_Good_200() {
+    this.reset()
+    this.`when`(
+        request().withMethod("GET").withPath("/users/good"),
+    ).respond(
+        response()
+            .withStatusCode(200)
+            .withBody("""[{ "id": 1296269, "name": "good", "owner": {} }]""")
+    )
+}
+
+fun ClientAndServer.scenario_Quirky() {
+    this.reset()
+    // quirky - use priority to simulate x2 500 errors followed by 200
+    this.`when`(
+        request()
+            .withMethod("GET").withPath("/users/quirky"),
+        Times.exactly(2), TimeToLive.exactly(SECONDS, 60L), 100
+    ).respond(
+        response()
+            .withStatusCode(500)
+    )
+
+    this.`when`(
+        request()
+            .withMethod("GET").withPath("/users/quirky"),
+        Times.exactly(2), TimeToLive.exactly(SECONDS, 60L), 1
+    ).respond(
+        response()
+            .withStatusCode(200)
+            .withBody("""[{ "id": 1296269, "name": "quirky", "owner": {} }]""")
+    )
 }
