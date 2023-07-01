316d885b81adf0c0ba1748dac5247514fa8dfe8c
David Soroko <david@picamail.eu>
2023-07-01 17:39:38 +0200

    BifidCipher 2

--- misc/src/main/kotlin/kstate/Turnstile.kt
+++
@@ -1,2 +0,0 @@
-package ru.nsk.samples.kstate
-
--- misc/src/main/kotlin/rosettacode/BifidCipher.kt
+++ misc/src/main/kotlin/rosettacode/BifidCipher.kt
@@ -1,77 +1,76 @@
 package rosettacode
 
+import kotlin.math.sqrt
+
 // https://rosettacode.org/wiki/Bifid_cipher
 //
-// ABCDEFGHIKLMNOPQRSTUVWXYZ
-//
-// ATTACKATDAWN
-// DQBDAXDQPDQH
 
-const val square = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-
-object Square {
-    private const val square: String = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-    private fun encode(ch: Char): Pair<Int, Int> = square.indexOf(ch).let { idx -> Pair(idx / 5, idx.mod(5)) }
-    fun encode(data: String): List<Pair<Int, Int>> = data.map { char -> encode(char) }
-    fun decode(pair: Pair<Int, Int>): Char = square[pair.first * 5 + pair.second]
-    fun decode(pair: List<Int>): Char = square[pair[0] * 5 + pair[1]]
+data class Square(private val square: String) {
+    private val dim: Int = sqrt(square.length.toDouble()).toInt()
+    fun encode(ch: Char): Pair<Int, Int> = square.indexOf(ch).let { idx -> Pair(idx / dim, idx.mod(dim)) }
+    fun decode(pair: List<Int>): Char = square[pair[0] * dim + pair[1]]
+    fun decode(row: Int, col: Int): Char = square[row * dim + col]
 }
 
-fun enc(data: String): String {
-    val listOfPairs = Square.encode(data).also { println("P1: $it") } // AT => [(0,0) , (3,3)
+
+class Bifid(private val sq: Square) {
+    fun encrypt(str: String): String {
+        fun expandAndScatter(str: String): IntArray {
+            val buffer = IntArray(str.length * 2)
+            str.forEachIndexed { i, ch ->
+                val (row, col) = sq.encode(ch)
+                buffer[i] = row
+                buffer[str.length + i] = col
+            }
+            return buffer
+        }
 
 
-    val pairOfLists = listOfPairs.fold(Pair(listOf<Int>(), listOf<Int>())) { acc, pair ->
-        Pair(acc.first + pair.first, acc.second + pair.second)
-    }.also { println("P2: $it") } // => ([0, 3..], [0, 3...)
+        val buffer = expandAndScatter(str)
 
-    val part3 = pairOfLists.first + pairOfLists.second
-    println("P3: $part3") // [0, 3, ... 0, 3 ...]
+        val characters: List<Char> = buffer.asIterable()
+            .windowed(size = 2, step = 2)
+            .map { sq.decode(it) }
 
-    return part3.windowed(size = 2, step = 2)
-        .map { Square.decode(it) }
-        .let { String(it.toCharArray()) }
-}
+        return String(characters.toCharArray())
+    }
 
-fun dec(data: String): String {
-    val d1 = Square.encode(data)
-        .fold(listOf<Int>()) { acc, pair -> acc + pair.first + pair.second }
-        .also { println("D1: $it") }
+    fun decrypt(str: String): String {
+        fun expand(str: String): IntArray {
+            val buffer = IntArray(str.length * 2)
+            for (i in buffer.indices step 2) {
+                val (row, col) = sq.encode(str[i / 2])
+                buffer[i] = row
+                buffer[1 + i] = col
+            }
+            return buffer
+        }
 
-    val d2 = d1.chunked(data.length).also { println("D2: $it") }
-    val d3 = d2[0].zip(d2[1]).also { println("D3: $it") }
-    val d4 = d3.map { Square.decode(it) }.also { println("D4: $it") }
-    return String(d4.toCharArray())
+        val buffer = expand(str)
+
+        val characters = str.toCharArray()
+        for (i in characters.indices) {
+            characters[i] = sq.decode(buffer[i], buffer[characters.size + i])
+        }
+        return String(characters)
+    }
 }
 
 
 fun main() {
+    val bifidABC_5x5 = Bifid(Square("ABCDEFGHIKLMNOPQRSTUVWXYZ"))
+    val bifidABC_6x6 = Bifid(Square("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
+    val bifidBGW_5x5 = Bifid(Square("BGWKZQPNDSIOAXEFCLUMTHYVR"))
 
-    enc("ATTACKATDAWN").also { println("> $it") }
-    dec("DQBDAXDQPDQH").also { println("> $it") }
-    check(dec("DQBDAXDQPDQH") == "ATTACKATDAWN")
+    bifidABC_5x5.encrypt("ATTACKATDAWN").also { println("$it") }
+    bifidABC_5x5.decrypt("DQBDAXDQPDQH").also { println("$it") }
 
-    enc("A").also { println("> $it") }
-    dec("A").also { println("> $it") }
+    bifidBGW_5x5.encrypt("FLEEATONCE").also { println("$it") }
+    bifidBGW_5x5.decrypt("UAEOLWRINS").also { println("$it") }
 
-    enc("AT").also { println("> $it") }
-    dec("DD").also { println("> $it") }
+    bifidBGW_5x5.encrypt("ATTACKATDAWN").also { println("$it") }
+    bifidBGW_5x5.decrypt("EYFENGIWDILA").also { println("$it") }
 
-//
-//    val part1 = "ATTACKATDAWN".map { char -> Square.encode(char) }
-//        .also { println(it) }
-//
-//    val part3 = part1.fold(Pair(listOf<Int>(), listOf<Int>())) { acc, pair ->
-//        Pair(acc.first + pair.first, acc.second + pair.second)
-//    }.also { println("part3 $it") }
-//
-//    val part4 = part3.first + part3.second
-//    println("part4 $part4")
-//
-//
-//
-//    part4.windowed(size = 2, step = 2)
-//        .map { Square.decode(it) }
-//        .joinToString(separator = "")
-//        .also { println(it) }
+    bifidABC_6x6.encrypt("THEINVASIONWILLSTARTONTHEFIRSTOFJANUARY").also { println("$it") }
+    bifidABC_6x6.decrypt("TBPDIPHJSPOTAIVMGPCZKNSCN09BFIHK64I7BM4").also { println("$it") }
 }
