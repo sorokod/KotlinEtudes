9ca59fe3e17b221ef898f881768279c7f6c5b7a6
David Soroko <david@picamail.eu>
2023-07-02 11:17:46 +0200

    splitOnChange doco

--- misc/src/main/kotlin/rosettacode/SplitOnChange.kt
+++ misc/src/main/kotlin/rosettacode/SplitOnChange.kt
@@ -1,25 +1,21 @@
 package rosettacode
 
-//https://rosettacode.org/wiki/Split_a_character_string_based_on_change_of_character#Kotlin
 
-fun splitOnChange(src: String): String =
-    src.fold("") { acc, c ->
-        if (acc.isEmpty() || acc.last() == c) "$acc$c" else "$acc, $c"
+/**
+ * Neat case of fold() being just the right tool.
+ *
+ * https://rosettacode.org/wiki/Split_a_character_string_based_on_change_of_character#Kotlin
+ */
+fun splitOnChange(str: String): String =
+    str.fold("") { acc, ch ->
+        if (acc.isEmpty() || acc.last() == ch) "$acc$ch" else "$acc, $ch"
     }
 
-
-fun splitOnChangeOriginal(s: String): String {
-    if (s.length < 2) return s
-    var t = s.take(1)
-    for (i in 1 until s.length)
-        if (t.last() == s[i]) t += s[i]
-        else t += ", " + s[i]
-    return t
-}
-
 fun main() {
-    splitOnChange("""gHHH5YY++///\""").also { println(it)}  // g, HHH, 5, YY, ++, ///, \
-//    val s = """gHHH5YY++///\"""
-//    println(splitOnChangeOriginal(s)) // g, HHH, 5, YY, ++, ///, \
-//    println(splitOnChange(s)) // g, HHH, 5, YY, ++, ///, \
+    val str = """gHHH5YY++///\"""
+
+    splitOnChange(str).also {
+        println(it)
+        check(it == """g, HHH, 5, YY, ++, ///, \""")
+    }
 }
