080f23f0947d2cae4e4241e68070f86b4ec0ad87
David Soroko <david@picamail.eu>
2024-04-14 10:57:25 +0200

    versions bump

---
+++ misc/src/main/kotlin/rosettacode/SmithNumbers.kt
@@ -0,0 +1,4 @@
+package rosettacode
+
+class SmithNumbers {
+}
---
+++ misc/src/test/kotlin/datastructs/BinaryArraySetTest.kt
@@ -0,0 +1,12 @@
+package datastructs 
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class BinaryArraySetTest : StringSpec({
+
+ "clear" { }
+
+"contains" { }
+
+"add" { } 
+})
