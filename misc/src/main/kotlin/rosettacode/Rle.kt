fb89f52da337dd7ced23079e097b1b82fe01cce5
David Soroko <david@picamail.eu>
2024-06-28 23:34:55 +0200

    Rle

--- misc/src/main/kotlin/rosettacode/Rle.kt
+++ misc/src/main/kotlin/rosettacode/Rle.kt
@@ -1,32 +1,12 @@
 package rosettacode
 
 /**
- * A recursive implementation of Run Length Encoding (RLE)
+ * A recursive implementation of Run Length Encoder (RLE)
  */
 
+fun rle(str: String): String {
+    if (str.isEmpty()) return ""
 
-tailrec fun rle(str: String, accumulated: String = ""): String =
-    when (str.isEmpty()) {
-        true -> accumulated
-        false -> {
-            val firstChar = str.first()
-            val count = str.stepWhile { it == firstChar }
-            rle(str.substring(count), accumulated + "$count$firstChar")
-        }
-    }
-
-/**
- * Step along the string as long as the [predicate] is true.
- *
- * @return the number of steps performed.
- */
-private fun String.stepWhile(predicate: (Char) -> Boolean): Int {
-    var i = 0
-    while (i in this.indices) {
-        when(predicate(this[i])) {
-            true -> i++
-            false -> return i
-        }
-    }
-    return i
+    val count = str.takeWhile { it == str.first() }.length
+    return "${count}${str.first()}" + rle(str.substring(count))
 }
