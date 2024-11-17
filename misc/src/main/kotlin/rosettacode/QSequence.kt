c8366a25288d5eefddd586320dd9482793c3dc3d
David Soroko <david@picamail.eu>
2024-11-17 19:18:35 +0100

    Hofstadter Q sequence

---
+++ misc/src/main/kotlin/rosettacode/QSequence.kt
@@ -0,0 +1,32 @@
+package rosettacode
+
+/**
+ * https://rosettacode.org/wiki/Hofstadter_Q_sequence
+ *
+ * Memoized version
+ */
+
+fun Q(n: Int): List<Int> {
+    val mem = mutableMapOf<Int, Int>().also {
+        it[1] = 1
+        it[2] = 1
+    }
+    val result = q(n, mem)
+    return mem.values.toList()
+}
+
+private fun q(n: Int, mem: MutableMap<Int, Int>): Int {
+    if (!mem.containsKey(n)) {
+        mem[n] =
+            q(n - q(n - 1, mem), mem) + q(n - q(n - 2, mem), mem)
+    }
+    return mem[n]!!
+}
+
+fun main() {
+    val n = 1000
+    Q(n).also { qList ->
+        println("Q[1..10] = ${qList.take(10)}")
+        println("Q($n)  = ${qList[1000 - 1]}") // 502
+    }
+}
