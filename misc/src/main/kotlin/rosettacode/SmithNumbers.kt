7228f61b191bd6aa565e994f0af9a07ad814753f
David Soroko <david@picamail.eu>
2025-04-18 21:02:44 +0200

    Smith numbers

--- misc/src/main/kotlin/rosettacode/SmithNumbers.kt
+++ misc/src/main/kotlin/rosettacode/SmithNumbers.kt
@@ -1,4 +1,29 @@
 package rosettacode
 
-class SmithNumbers {
+import org.apache.commons.math3.primes.Primes
+
+//https://rosettacode.org/wiki/Smith_numbers#Kotlin
+
+fun sumDigits(n: Int): Int =
+    n.toString().map { it.digitToInt() }.sum()
+
+fun isSmith(n: Int): Boolean {
+    if (n < 2) return false
+    val factors = Primes.primeFactors(n)
+
+    return when (factors.size) {
+        1 -> false // primes don't count
+        else -> sumDigits(n) == factors.sumOf { sumDigits(it) }
+    }
+}
+
+fun main(args: Array<String>) {
+    println("The Smith numbers below 10000 are:\n")
+
+    val smiths = (2..9999)
+        .filter { isSmith(it) }
+        .onEach { print("%5d".format(it)) }
+
+    println("\n\n${smiths.size} numbers found (should be 376)")
+
 }
